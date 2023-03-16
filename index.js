c53d8dd401f93d085e8b162edf38d6d496742b62
Bradigson Nuñez <Bradigson.Nunez@dpworld.com.do>
2023-03-15 23:44:13 -0400

    my-chat-backend-websocket

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const SocketServer = require('socket.io');
+const http = require('http');
+const cors = require('cors');
+
+const app = express();
+
+//socket configuration
+const server = http.createServer(app);
+
+const io = SocketServer(server, {
+    cors:{
+        origin:'*'
+    }
+});
+
+
+
+io.on('connection', (socket)=>{
+
+    socket.on('message_sent', (message)=>{
+        console.log(message);
+
+        socket.emit("receive_message", message);
+    })
+});
+
+
+app.set('port', process.env.PORT || 5000);
+app.use(express.json());
+
+
+server.listen(app.get('port'), ()=>{
+    console.log('server available at : ', app.get('port'));
+})
